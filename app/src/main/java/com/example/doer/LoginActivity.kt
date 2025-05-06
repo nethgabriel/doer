7a9bf8d3a64e7a487c824c6bca3faeb424db871d
JL Timon <balloydia@gmail.com>
2025-05-06 23:16:08 +0800

    Sign Up Button Overlay

--- app/src/main/java/com/example/doer/LoginActivity.kt
+++ app/src/main/java/com/example/doer/LoginActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,22 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val signUpButton = findViewById<Button>(R.id.btn_sign_up)
+        val signUpOverlay = findViewById<View>(R.id.sign_up_overlay)
+       // val closeButton = signUpOverlay.findViewById<Button>(R.id.btn_close)
+
+        signUpButton.setOnClickListener {
+            signUpOverlay.visibility = View.VISIBLE
+        }
+
+      /*  closeButton.setOnClickListener {
+            signUpOverlay.visibility = View.GONE
+        }*/
+
+
+
+
+
     }
 }
